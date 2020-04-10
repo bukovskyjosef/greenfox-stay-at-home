2f9f532a3f2f9533f617f19eb71b31d9c58383d5
Josef Bukovský <bukovskyjosef@gmail.com>
2020-04-10 16:33:48 +0200

    First meaningful content

---
+++ scripts.js
@@ -0,0 +1,30 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav-bar").style.top = "0";
+    } else {
+        document.getElementById("nav-bar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+document.getElementById("nav-bar").onmouseover = function () {
+    mouseOver();
+};
+document.getElementById("nav-bar").onmouseout = function () {
+    mouseOut();
+};
+
+function mouseOver() {
+    document.getElementById("nav-bar").style.top = "0";
+}
+
+function mouseOut() {
+    if (window.pageYOffset == 0) {
+    } else {
+        document.getElementById("nav-bar").style.top = "-50px";
+    }
+}
